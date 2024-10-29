dd938e05b6858f92dc000c029f6c0b9d5ba778db
Henrrike Quarshie <henrrikequarshie@gmail.com>
2024-10-29 02:03:54 +0000

    starting all over

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+
+import './App.css'
+import About from './components/About'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Resume from './components/Resume'
+import Services from './components/Services'
+import Skills from './components/Skills'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Navbar />
+     <About />
+     <Resume />
+     <Skills />
+     <Services />
+     <Footer />
+    </>
+  )
+}
+
+export default App
